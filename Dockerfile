519d5cea2d5300ba09955f0ad5f4ebe02748ffcb
chako-mountain <gonzaburouduanyewufu@gmail.com>
2025-12-06 16:07:49 +0900

    データベース接続

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,18 @@
-# ベースイメージを指定
-FROM golang:1.25-alpine AS builde
+# FROM golang:1.25-alpine AS builde
 
-# air をインストール（リポジトリ変更済み）
-RUN go install github.com/air-verse/air@latest
+# # air をインストール（リポジトリ変更済み）
+# RUN go install github.com/air-verse/air@latest
 
-# ワーキングディレクトリを設定
-WORKDIR /app
+# # ワーキングディレクトリを設定
+# WORKDIR /app
 
-# Goモジュールを取得するためにgo.modとgo.sumをコピー
-COPY go.mod go.sum ./
-ENV GOPROXY=https://goproxy.cn,https://goproxy.io,direct 
-RUN go mod download
+# # Goモジュールを取得するためにgo.modとgo.sumをコピー
+# COPY go.mod go.sum ./
+# ENV GOPROXY=https://goproxy.cn,https://goproxy.io,direct 
+# RUN go mod download
 
-# アプリケーションのソースコードをコピー
-COPY . .
+# # アプリケーションのソースコードをコピー
+# COPY . .
 
-# アプリケーションをビルド
-RUN go build -o main .
-CMD ["./main"]
+# # アプリケーションをビルド
+# RUN go build -o main .
--- main.go
+++ main.go
@@ -1,13 +1,92 @@
+// package main
+
+// import (
+// 	"database/sql"
+// 	"fmt"
+
+// 	"github.com/gin-gonic/gin"
+// 	// "fmt"
+// 	// "time"
+// )
+
+// func main() {
+// 	router := gin.Default()
+
+// 	db, err := sql.Open("mysql", "docker:docker@tcp(localhost:3305)/test_database?parseTime=true&loc=Asia%2FTokyo")
+// 	if err != nil {
+// 		fmt.Print(err)
+// 	}
+// 	defer db.Close()
+
+// 	router.GET("/ping", func(c *gin.Context) {
+// 		c.JSON(200, gin.H{
+// 			"message": "pong",
+// 		})
+// 	})
+// 	router.Run()
+// }
+
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"database/sql"
+	"log"
+	"reflect"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	// "/tutorial"
+	// "github.com/username/kitta_backend/tutorial"
+	"kitta_backend/tutorial"
+)
+
+func run() error {
+	ctx := context.Background()
+
+	// db, err := sql.Open("mysql", "docker:docker@/test_database?parseTime=true")
+	db, err := sql.Open("mysql", "docker:docker@tcp(localhost:3305)/test_database?parseTime=true&loc=Asia%2FTokyo")
+	if err != nil {
+		return err
+	}
+
+	queries := tutorial.New(db)
+
+	// list all authors
+	authors, err := queries.ListAuthors(ctx)
+	if err != nil {
+		return err
+	}
+	log.Println(authors)
+
+	// create an author
+	result, err := queries.CreateAuthor(ctx, tutorial.CreateAuthorParams{
+		Name: "Brian Kernighan",
+		Bio:  sql.NullString{String: "Co-author of The C Programming Language and The Go Programming Language", Valid: true},
+	})
+	if err != nil {
+		return err
+	}
+
+	insertedAuthorID, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+	log.Println(insertedAuthorID)
+
+	// get the author we just inserted
+	fetchedAuthor, err := queries.GetAuthor(ctx, insertedAuthorID)
+	if err != nil {
+		return err
+	}
+
+	// prints true
+	log.Println(reflect.DeepEqual(insertedAuthorID, fetchedAuthor.ID))
+	return nil
+}
 
 func main() {
-	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	router.Run() // デフォルトで0.0.0.0:8080で待機します
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
 }
